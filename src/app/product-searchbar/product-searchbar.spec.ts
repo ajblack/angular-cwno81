95fe1f80021a1c11559c6593e95ef94a49315dd9
ajblack <austin.blackman@gmail.com>
2020-06-11 10:23:47 -0400

    directory change for tests

--- src/app/product-searchbar/product-searchbar.spec.ts
+++ src/app/product-searchbar/product-searchbar.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement }  from '@angular/core';
-import { ProductSearchbarComponent } from './product-searchbar/product-searchbar.component';
+import { ProductSearchbarComponent } from './product-searchbar.component';
 import { FormsModule } from '@angular/forms';
 
 //
